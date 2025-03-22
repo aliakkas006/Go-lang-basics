738433740169c0ea4a1b15d3234544538571ccd4
Ali Akkas <aliakkas006@gmail.com>
2025-03-22 14:34:13 +0600

    feat: add closure

---
+++ closure/main.go
@@ -0,0 +1,71 @@
+package main
+
+import "fmt"
+
+const a = 10
+
+var p = 100
+
+func outer() func() {
+	money := 500
+	age := 24
+
+	fmt.Println("Age: ", age)
+
+	show := func() {
+		money = money + a + p
+		fmt.Println("Money: ", money)
+	}
+
+	return show
+}
+
+func call() {
+	incr_1 := outer()
+	incr_1()
+	incr_1()
+
+	fmt.Println("---- Second incremetnt ------")
+
+	incr_2 := outer()
+	incr_2()
+	incr_2()
+}
+
+func main(){
+	call()
+}
+
+func init(){
+	fmt.Println("==== Bank ====")
+}
+
+/* 
+	2 phases:
+		1. Compilation phase (compile time)
+		2. Execution phase (run time)
+
+
+
+	************  Compilation Phase (compile time)  ***************
+
+		### Code Segment (readonly => (const and function)) ###
+
+			- a
+			- outer = func() {...}
+			- outerAnonymous_1 = func() {...}
+			- call = func() {...}
+			- main = func() {...}
+			- init = func() {...}
+
+	
+
+	************  Execution Phase (run time)  ***************
+			- p
+
+
+
+
+	go build main.go -> compile it -> main 
+	./main
+*/
