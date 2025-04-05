52249247f2785b1deb625a03cee1976a323fd3e2
Ali Akkas <aliakkas006@gmail.com>
2025-04-05 11:05:52 +0600

    add arrays

---
+++ arrays/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+var arr_3 = [2]string{"Ali", "Akkas"}
+
+func main() {
+	var arr [3]int
+
+	arr[0] = 1
+	arr[1] = 2
+
+	fmt.Println(arr)
+
+	// Short-hand declaration:
+	arr_2 := [3]int{1, 2, 3}
+
+	fmt.Println(arr_2)
+
+	fmt.Println(arr_3)
+}
